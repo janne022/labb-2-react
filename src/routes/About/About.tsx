19cf170a528990f482b616fefc5307d63b464adb
Johannes Flodin <jannis020609@gmail.com>
2026-03-22 21:25:52 +0100

    this was a hard one

--- src/routes/About/About.tsx
+++ src/routes/About/About.tsx
@@ -1,3 +1,15 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="flex flex-col min-h-screen gap-20 bg-background text-foreground pb-20">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold mb-6 text-primary">About Me</h1>
+        <p className="text-lg leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quod
+          voluptatem soluta repudiandae, perspiciatis tenetur minus sint
+          nesciunt qui, provident fuga dicta! Libero, totam impedit. Unde vero
+          nisi obcaecati quaerat.
+        </p>
+      </div>
+    </div>
+  );
 }
